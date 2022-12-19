d0966a7f921725a1cc970547573f511061729eb6
Lautaro Moor <lautimoor2@gmail.com>
2022-12-19 20:46:51 -0300

    Cambio

--- funciones.py
+++ funciones.py
@@ -1,19 +1,19 @@
 import json
 
 #Leer JSONs
-with open ('jsons/usuarios.json','r') as archivoJson:
+with open ('jsons/usuarios.json','r',encoding="utf8") as archivoJson:
     usuarios = json.load(archivoJson)
 
-with open ('jsons/peliculas.json','r') as archivoJson:
+with open ('jsons/peliculas.json','r',encoding="utf8") as archivoJson:
     peliculas = json.load(archivoJson)
 
-with open ('jsons/directores.json','r') as archivoJson:
+with open ('jsons/directores.json','r',encoding="utf8") as archivoJson:
     directores = json.load(archivoJson)
 
-with open ('jsons/generos.json','r') as archivoJson:
+with open ('jsons/generos.json','r',encoding="utf8") as archivoJson:
     generos = json.load(archivoJson)
 
-with open ('jsons/comentarios.json','r') as archivoJson:
+with open ('jsons/comentarios.json','r',encoding="utf8") as archivoJson:
     comentarios = json.load(archivoJson)
 
 #Pasando a JSONs
